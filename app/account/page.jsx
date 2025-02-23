aad3fb3ce037122c0f24231d8ba99cc7c0d7fff8
Arthur <arthur@Arthurs-Mac-mini.local>
2025-02-23 15:21:29 -0700

    restarting vc

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -37,7 +37,7 @@ export default function Page() {
   return (
     <div className='w-full flex flex-col items-center justify-center'>
       <Hero title="Your Account" description="Manage all your sessions from here"/>
-      <Information data={compiledData}/>
+      <Information data={compiledData}/> 
     </div>
   )
 }
