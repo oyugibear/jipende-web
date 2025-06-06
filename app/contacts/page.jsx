92d4ce1ada8bf485c5eed40215d12ce6c25ae641
arthur oduor <arthuroduor@arthurs-Mac-mini.local>
2025-06-05 22:53:43 -0700

    did a few changes with rubie - modifications grammar

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -2,6 +2,7 @@
 
 import BasicInputs from '@/components/Constants/fields/BasicInputs'
 import BasicTextArea from '@/components/Constants/fields/BasicTextArea'
+import PictureHero from '@/components/Constants/PictureHero'
 import BasicQuery from '@/components/Constants/Queries/BasicQuery'
 import { Basic } from 'next/font/google'
 import Image from 'next/image'
@@ -10,32 +11,24 @@ import { MdOutlineAccessTime, MdOutlineEmail, MdOutlinePhoneLocked } from 'react
 
 export default function page() {
   return (
-    <div className='flex flex-col w-full'>
-      <div className='w-full h-full relative'>
-          <div className='absolute flex flex-col items-center justify-center w-full h-full z-10 font-medium text-white px-4'>
-            <h1 className='text-3xl md:text-5xl text-start'>Contact Us </h1>
-            <p className='text-center max-w-[600px] mt-2 font-medium'>
-              Contact Us Directly For More Information We&apos;ll Get Back To You ASAP
-            </p>
-            <div className='my-4 flex flex-col gap-8 md:my-4 text-lg text-center md:flex-row md:items-start justify-center md:justify-between'>
-              <div className='flex flex-col justify-center gap-2 items-center'>
-                  <MdOutlinePhoneLocked size={40} />
-                  <p > +254 745601992 || +254 735432045</p>
-              </div>
-              <div className='flex flex-col justify-center gap-2 items-center'>
-                  <MdOutlineEmail size={40} />
-                  <p > info@africajipendewellness.com</p>
-              </div>
-              <div className='flex flex-col justify-center gap-2 items-center'>
-                  <MdOutlineAccessTime size={40} />
-                  <p > Monday – Friday: 9:00am – 6:00pm</p>
-                  <p > Saturday: 8:00am – 1:00pm</p>
-              </div>
-            </div>
-            
+    <div className='w-full flex flex-col items-center justify-center'>
+      <PictureHero title='Contact Us' description="" imgPath='/assets/contacts/c4.jpg' />
+      <div className='bg-yellow-400 w-full py-2'>
+        <div className='max-w-[1440px] w-full flex flex-wrap items-start justify-center p-4 gap-6'>
+          <div className='min-w-[280px] flex flex-col justify-center gap-2 items-center'>
+            <MdOutlinePhoneLocked size={20} />
+            <p className='font-bold'> +254 745601992 <br /> +254 735432045</p>
           </div>
-          <div className='absolute w-full h-full bg-black/60' />
-          <Image src='/assets/contacts/c4.jpg' alt='happy person' width={1920} height={568} className='h-[800px] max-h-[800px] xl:max-h-[1000px] object-cover object-center w-full'/>
+          <div className='min-w-[280px] flex flex-col justify-center gap-2 items-center text-center'>
+            <MdOutlineAccessTime size={20} />
+            <p className='font-bold'> Monday – Friday: <br /> 9:00am – 6:00pm</p>
+            <p className='font-bold'> Saturday: <br /> 8:00am – 1:00pm</p>
+          </div>
+          <div className='min-w-[280px] flex flex-col justify-center gap-2 items-center'>
+            <MdOutlineEmail size={20} />
+            <p className='font-bold'> info@africajipendewellness.com</p>
+          </div>
+        </div>
       </div>
 
       <BasicQuery />
