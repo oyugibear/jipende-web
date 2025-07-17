c4cd637ede2e310e56ff5fe91fe7fa37ce3386ca
arthur oyugi <arthuroyugi@arthurs-MacBook-Pro.local>
2025-07-17 12:41:46 -0700

    final changes for now

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -32,11 +32,17 @@ export default  function Page() {
     }, []);
     
     const therapyItems = [
-        { key: '1', label: 'Individual Awareness', value: 'Individual Awareness' },
-        { key: '2', label: 'Self Development', value: 'Self Development' },
-        { key: '3', label: 'Mental Awareness', value: 'Mental Awareness' },
-        { key: '4', label: 'After Care', value: 'After Care' },
-        { key: '5', label: 'All', value: 'All' },
+        // { key: '1', label: 'Individual Awareness', value: 'Individual Awareness' },
+        // { key: '2', label: 'Self Development', value: 'Self Development' },
+        // { key: '3', label: 'Mental Awareness', value: 'Mental Awareness' },
+        { key: '1', label: 'Individual', value: 'Individual' },
+        { key: '2', label: 'Corporate', value: 'Corporate' },
+        { key: '3', label: 'Group', value: 'Group' },
+        { key: '4', label: 'Family', value: 'Family' },
+        { key: '5', label: 'School', value: 'School' },
+        { key: '6', label: 'Friends', value: 'Friends' },
+        { key: '7', label: 'Couples', value: 'Couples' },
+        { key: '8', label: 'All', value: 'All' },
     ];
 
     const locationItems = [
